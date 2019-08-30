5717cedadf43743386dcd7ee119debe6d12533d2
Miquel Puig <mpuig22@gmail.com>
2019-08-30 21:49:15 +0200

    carcassa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { YoutubeService } from './services/youtube.service';
 
 // AIzaSyA13zooCciT7Tq9FTI5TzdqkBWdXtrMsm0
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [YoutubeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
